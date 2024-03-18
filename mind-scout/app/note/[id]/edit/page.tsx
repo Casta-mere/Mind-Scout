a909b64614d2d4cc21cbf47255dcc1fa29bf931b
Casta-mere <castamerego@gmail.com>
2024-03-18 17:16:26 +0800

    Add Edit Note page

---
+++ mind-scout/app/note/[id]/edit/page.tsx
@@ -0,0 +1,23 @@
+import dynamic from "next/dynamic";
+import prisma from "@/prisma/client";
+import { notFound } from "next/navigation";
+const NoteForm = dynamic(() => import("@/app/note/_components/NoteForm"), {
+  ssr: false,
+});
+
+interface Props {
+  params: { id: string };
+}
+
+const EditPage = async ({ params }: Props) => {
+  const note = await prisma.page.findUnique({
+    where: {
+      id: params.id,
+    },
+  });
+
+  if (!note) notFound();
+
+  return <NoteForm note={note} />;
+};
+export default EditPage;
